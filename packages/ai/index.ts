c78ff90087c645861984f61fbca0199d5a2d8e75
TROTRO <63386366+tro-lee@users.noreply.github.com>
2025-05-09 00:33:03 +0800

    zancun

--- packages/ai/index.ts
+++ packages/ai/index.ts
@@ -1,10 +1,1 @@
-import * as fs from 'node:fs'
-import { addDocuments } from './src/common'
-import { getExplainCodeGraph } from './src/explain-code-graph/graph'
-
-const documentsJSON = fs.readFileSync('./dist/docs.json', 'utf-8')
-const documents = JSON.parse(documentsJSON)
-addDocuments(documents)
-
-export const app = await getExplainCodeGraph()
-app.name = 'ExplainFunctionGraph'
+export * from './src/explain-code-graph/graph'
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,8 @@
 import * as fs from 'node:fs'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { streamText } from 'hono/streaming'
+import { getExplainCodeGraph } from 'module-toolbox-ai'
 import {
   scanEntryFilePathsByDir,
   transformFilePathsToCoreData,
@@ -83,6 +85,45 @@ app.get('/code-by-location', async (c) => {
   return c.json({
     status: 'success',
     data: content,
+  })
+})
+
+app.get('/explain-code-by-location-stream', async (c) => {
+  const filepath = c.req.query('filepath') || ''
+  const locStart = Number(c.req.query('locStart') || '')
+  const locEnd = Number(c.req.query('locEnd') || '')
+
+  if (!filepath || Number.isNaN(locStart) || Number.isNaN(locEnd)) {
+    return c.json({
+      status: 'error',
+      message: 'filepath, locStart and locEnd are required',
+    }, 400)
+  }
+
+  const code = fs.readFileSync(filepath, 'utf-8')
+  const content = code.slice(locStart, locEnd)
+
+  // 创建流式响应
+  return streamText(c, async (stream) => {
+    try {
+      const graph = await getExplainCodeGraph()
+      const responseStream = graph.stream({
+        messages: [['user', content]],
+      }, {
+        streamMode: 'messages',
+      })
+
+      for await (const responses of await responseStream) {
+        await stream.write(responses[0].content)
+        process.stdout.write(responses[0].content)
+      }
+    }
+    catch (err) {
+      // 处理未知错误类型
+      const error = err as Error
+      // 发送错误消息
+      await stream.write(`data: {"status":"error", "message": ${JSON.stringify(error?.message || '未知错误')}}\n\n`)
+    }
   })
 })
 
